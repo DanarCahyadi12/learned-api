1103761a2b5dfe9fb3eb32c9fa4948233c61b423
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-15 14:20:24 +0800

    refactor: skipping auth for auth class

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,12 +14,12 @@ import { AuthDto } from './DTOs';
 import { Response } from 'express';
 import { RefreshTokenGuard } from './guards';
 import { Cookies, SkipAuth } from './decorators';
+@SkipAuth()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  @SkipAuth()
   @HttpCode(HttpStatus.OK)
   async signIn(@Body() dto: AuthDto, @Res() res: Response) {
     const response = await this.authService.signIn(dto, res);
@@ -27,7 +27,6 @@ export class AuthController {
   }
 
   @Get('token')
-  @SkipAuth()
   @UseGuards(RefreshTokenGuard)
   async getRefreshToken(
     @Request() req,
