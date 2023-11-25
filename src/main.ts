bfc1290653e002614cec1a51034226d44cda85d6
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-25 16:03:38 +0800

    adding global pipes

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as path from 'path';
 import { copyIntoDistFolder } from './utils';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+  app.useGlobalPipes(new ValidationPipe());
   copyIntoDistFolder();
   app.use(cookieParser());
   await app.listen(process.env.SERVER_PORT);
