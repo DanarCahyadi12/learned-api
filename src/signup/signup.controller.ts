b93bb6526125ec4555c2472bb9318d1bb0cc8add
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-25 15:38:23 +0800

    fix: skip auth for sign up

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignUpDto } from './DTOs';
+import { SkipAuth } from 'src/auth/decorators';
 
+@SkipAuth()
 @Controller('signup')
 export class SignupController {
   constructor(private readonly signUpService: SignupService) {}
