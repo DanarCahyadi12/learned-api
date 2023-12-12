3053fa5e7a1c0915c58fc2ed84f657a00fc6321f
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-12-12 14:19:22 +0800

    adding next url pagination

--- src/utils/get-next-url.ts
+++ src/utils/get-next-url.ts
@@ -1,9 +1,8 @@
 export function getNextUrl(
-  totalData: number,
+  totalPage: number,
   take: number,
   page: number,
 ): string {
-  const totalPage: number = Math.ceil(totalData / take);
   return page >= totalPage
     ? null
     : `${process.env.BASE_URL}/classroom/created?page=${page + 1}&take=${take}`;
