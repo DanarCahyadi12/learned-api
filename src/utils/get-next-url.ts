7a3dbcfabb97ea14cebafcedab8bb8e9099f6b23
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-12-22 16:05:42 +0800

    fix: dynamic url

--- src/utils/get-next-url.ts
+++ src/utils/get-next-url.ts
@@ -2,8 +2,9 @@ export function getNextUrl(
   totalPage: number,
   take: number,
   page: number,
+  path: string,
 ): string {
-  return page >= totalPage
+  return page >= totalPage || take === 0
     ? null
-    : `${process.env.BASE_URL}/classroom/created?page=${page + 1}&take=${take}`;
+    : `${process.env.BASE_URL}/${path}?page=${page + 1}&take=${take}`;
 }
--- src/utils/get-prev-url.ts
+++ src/utils/get-prev-url.ts
@@ -1,5 +1,5 @@
-export function getPrevUrl(page: number, take: number): string {
+export function getPrevUrl(page: number, take: number, path: string): string {
   return page > 1
-    ? `${process.env.BASE_URL}/classroom/created?page=${page - 1}&take=${take}`
+    ? `${process.env.BASE_URL}/${path}?page=${page - 1}&take=${take}`
     : null;
 }
