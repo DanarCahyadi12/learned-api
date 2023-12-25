940a079b2a8c7039901c3459a4d1ee05f570d03b
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-12-25 16:42:33 +0800

    migration

---
+++ prisma/migrations/20231225082743_add_filename_field_at_material_files_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `material_files` ADD COLUMN `filename` VARCHAR(191) NOT NULL DEFAULT ' ';
---
+++ prisma/migrations/20231225082848_delete_default_value_in_filename_field_at_material_fiels_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `material_files` ALTER COLUMN `filename` DROP DEFAULT;
